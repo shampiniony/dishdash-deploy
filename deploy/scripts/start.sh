cbf3e17a43444c9a6101e7d295c98daf01678b27
AlexDyakonov <shram.monolit@mail.ru>
2024-05-19 04:40:04 +0300

    Added separated composes and start scipt

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-docker-compose up --build -d
+./create_networks.sh
+./frontend_docker.sh
+./backend_docker.sh
+
+
+echo "Preparing and starting frontend..."
+(cd ../../dish-dash-frontend && docker-compose up -d)
+echo "Frontend started."
+
+echo "Preparing and starting backend..."
+(cd ../../dishdash-server && docker-compose up -d)
+echo "Backend started."
+
+echo "Starting traefik services..."
+docker-compose up -d
+echo "Global traefik started."
